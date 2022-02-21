fc22a4a07857b46e236420658df70f0588441a28
peterroe <peterroe@163.com>
2022-02-21 13:45:59 +0800

    fix style

--- demo/index.ts
+++ demo/index.ts
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
-import UWaterfall from '../src/main'
+// import UWaterfall from '../src/main'
+import UWaterfall from 'uwaterfall'
+// import 'uwaterfall/dist/style.css'
 import test from './test.vue'
 
 createApp(test).use(UWaterfall).mount('#app')
