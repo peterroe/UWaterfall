aedbf1980abf14dd0a0ab1df6b91a34be7d75cdc
peterroe <peterroe@163.com>
2022-02-21 14:28:35 +0800

    update

--- demo/test.vue
+++ demo/test.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 const imgArr = ref(new Array(20).fill(0).map((_,i) => `./demo/assets/${i + 1}.jpg`))
-
 </script>
 
 
@@ -10,9 +9,9 @@ const imgArr = ref(new Array(20).fill(0).map((_,i) => `./demo/assets/${i + 1}.jp
     <h3>Demo:</h3>
     <UWaterfall :columnCount="4" columnGap="10px">
       <img 
-        v-for="item in imgArr" 
+        v-for="(item, index) in imgArr" 
         :key="item" 
-        :src="item"
+        :src="'/img/'+ (index + 1) + '.jpg'"
       />
     </UWaterfall>
   </div>
