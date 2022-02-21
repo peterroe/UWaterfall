49695e16da0aa1b424d1d84d8ca9e2ce22a23d5d
peterroe <peterroe@163.com>
2022-02-21 13:15:41 +0800

    update

--- demo/test.vue
+++ demo/test.vue
@@ -1,7 +1,19 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+const imgArr = ref(new Array(20).fill(0).map((_,i) => `./demo/assets/${i + 1}.jpg`))
+
+</script>
+
+
 <template>
   <div>
-    <div>hello</div>
-    <UWaterfall></UWaterfall>
+    <h3>Demo:</h3>
+    <UWaterfall>
+      <img 
+        v-for="item in imgArr" 
+        :key="item" 
+        :src="item"
+      />
+    </UWaterfall>
   </div>
-  
 </template>
--- src/UWaterfall.vue
+++ src/UWaterfall.vue
@@ -1,3 +1,38 @@
-<template>
-  <div>d</div>
-</template>
+<script lang="tsx">
+import { defineComponent, computed } from 'vue'
+
+export default defineComponent({
+  props: {
+    columnCount: {
+      type: Number,
+      default: 3
+    },
+    columnGap: {
+      type: String,
+      default: '15px'
+    }
+  },
+  setup(props, { slots }) {
+    const style = computed(() => {
+      return {
+        'columnCount': props.columnCount,
+        'columnGap': props.columnGap
+      }
+    })
+    return () => (
+      <div class="u-waterfall" style={style.value}>
+        {slots.default?.()}
+      </div>
+    )
+  }
+})
+</script>
+
+<style>
+.u-waterfall img{
+  width: 100%;
+  height: 100%;
+  object-fit: fill;
+  break-inside: avoid;
+}
+</style>
